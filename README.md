8d8b2fb9ded99d6c2bf008d03eda87b40456db77
uddavdhakal190 <uddavdhakal190@gmail.com>
2025-12-24 19:19:42 +0200

    Initial commit - simple age calculator

---
+++ README.md
@@ -0,0 +1,19 @@
+# Age Calculator
+
+A simple age calculator built using HTML, CSS, and JavaScript.
+
+## Features
+- Select date of birth
+- Calculates age in years
+- Clean and simple UI
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## How to Run
+1. Clone the repository
+2. Open `index.html` in browser
+
+
---
+++ script.js
@@ -0,0 +1,36 @@
+function calculateAge() {
+    const dob = document.getElementById("dob").value;
+
+    if (dob === "") {
+        document.getElementById("result").innerText = "Please select a date.";
+        return;
+    }
+
+    const birthDate = new Date(dob);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    if (
+        monthDifference < 0 ||
+        (monthDifference === 0 && today.getDate() < birthDate.getDate())
+    ) {
+        age--;
+    }
+
+    document.getElementById("result").innerText =
+        `You are ${age} years old.`;
+}
+
+
+
+ /* 
+ document.getElementById()	Gets HTML element
+ new Date(dob)	                Converts input to date
+getFullYear()	                Gets year
+getMonth()	                    Gets month (0–11)
+getDate()	                    Gets day
+age--	                        Adjusts if birthday not yet passed 
+*/
